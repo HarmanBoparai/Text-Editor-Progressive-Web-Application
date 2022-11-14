f43e7e516f2d4a1a25d9b174457fdbea8e43443e
HarmanBoparai <erharmanboparai@gmail.com>
2022-11-14 17:50:59 -0500

    written logic that gets all the content from the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -2,6 +2,7 @@ import { openDB } from 'idb';
 
 const initdb = async () =>
   openDB('jate', 1, {
+    
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
@@ -13,9 +14,23 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) =>{
+
+} 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () =>{
+  
+  console.log('GET from the database');
+   const contactDb = await openDB('jate', 1);
+  const tx = contactDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+   const request = store.getAll();
+
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
 
 initdb();
